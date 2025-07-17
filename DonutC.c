aca5f4cdf394e6fdf57e907a375af1ced8177106
LatifKrasniqi24 <196327957+LatifKrasniqi24@users.noreply.github.com>
2025-07-17 22:55:04 +0200

    ajout de la position du donut et d'une condition pour que le donut ne depasse pas les limites

--- DonutC.c
+++ DonutC.c
@@ -4,6 +4,8 @@
 
 #define colonne 80
 #define ligne 24
+#define ligne_centre ligne/2
+#define colonne_centre colonne/2
 
 int main(void)
 {
@@ -15,9 +17,14 @@ int main(void)
     double x;
     double y;
     double z;
+    int xp = 0;
+    int yp = 0;
+    double facteur_zoom = colonne_centre / (R + r);
 
     // delcaration des tableaux
     char screen[ligne][colonne];
+
+    printf("\x1b[H");
 
     // initialisation du tableau avec des espaces
     for (int x = 0; x < ligne; x++) {
@@ -27,16 +34,25 @@ int main(void)
     }
 
     // calcul des points theta et phi
-    for (double a = 0; a <= (2 * M_PI); a += 0.5) {
+    for (double a = 0; a <= (2 * M_PI); a += 0.07) {
         theta = a;
 
-        for (double b = 0; b <= (2 * M_PI); b += 0.5) {
+        for (double b = 0; b <= (2 * M_PI); b += 0.07) {
             phi = b;
 
-            //clacule du petit cercle (r) qui tourne autours du grand cercle (R) formant un tore (donut)
+            //clacule du petit cercle (r) qui tourne autours du grand cercle (R) forma[nt un tore (donut)
             x = (R + r * cos(theta)) * cos(phi);
             y = (R + r * cos(theta)) * sin(phi);
             z = r * sin(theta);
+
+            // position du donut
+            xp = (int)(x * facteur_zoom + colonne_centre);
+            yp = (int)(y * facteur_zoom + ligne_centre);
+
+            // condition pour que le donut reste dans le tableau screen et ajout d'un caractère
+            if (xp >= 0 && xp < colonne && yp >= 0 && yp < ligne){
+                screen[yp][xp] = '@';
+            }
         }
     }
 
