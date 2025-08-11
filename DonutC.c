ae075f06e00e028152218296dcfb860b47b1037e
LatifKrasniqi24 <196327957+LatifKrasniqi24@users.noreply.github.com>
2025-08-11 22:41:50 +0200

    ajout de la 3d, luminosité, animation. (j'ai pas push depuis longtemp parce que j'etais trop focus pour push a chaque ajout)

--- DonutC.c
+++ DonutC.c
@@ -19,48 +19,101 @@ int main(void)
     double z;
     int xp = 0;
     int yp = 0;
-    double facteur_zoom = colonne_centre / (R + r);
+    double facteur_zoom = (colonne_centre / (R + r)) * 2.0;
+    double A = 0; 
+    double B = 0;
 
-    // delcaration des tableaux
-    char screen[ligne][colonne];
+    for(;;) {
+        // delcaration des tableaux
+        char screen[ligne][colonne];
+        double zbuffer[ligne][colonne];
 
-    printf("\x1b[H");
+        printf("\x1b[H");
 
-    // initialisation du tableau avec des espaces
-    for (int x = 0; x < ligne; x++) {
-        for (int y = 0; y < colonne; y++) {
-            screen[x][y] = ' ';
-        }
-    }
-
-    // calcul des points theta et phi
-    for (double a = 0; a <= (2 * M_PI); a += 0.07) {
-        theta = a;
-
-        for (double b = 0; b <= (2 * M_PI); b += 0.07) {
-            phi = b;
-
-            //clacule du petit cercle (r) qui tourne autours du grand cercle (R) forma[nt un tore (donut)
-            x = (R + r * cos(theta)) * cos(phi);
-            y = (R + r * cos(theta)) * sin(phi);
-            z = r * sin(theta);
-
-            // position du donut
-            xp = (int)(x * facteur_zoom + colonne_centre);
-            yp = (int)(y * facteur_zoom + ligne_centre);
-
-            // condition pour que le donut reste dans le tableau screen et ajout d'un caractère
-            if (xp >= 0 && xp < colonne && yp >= 0 && yp < ligne){
-                screen[yp][xp] = '@';
+        // initialisation du tableau avec des espaces
+        for (int x = 0; x < ligne; x++) {
+            for (int y = 0; y < colonne; y++) {
+                screen[x][y] = ' ';
+                zbuffer[x][y] = -INFINITY;
             }
         }
-    }
 
-    // affichage du tableau
-    for (int i = 0; i < ligne; i++) {
-        for (int j = 0; j < colonne; j++) {
-            putchar(screen[i][j]);
+        // calcul des points theta et phi
+        for (double a = 0; a <= (2 * M_PI); a += 0.07) {
+            theta = a;
+
+            for (double b = 0; b <= (2 * M_PI); b += 0.07) {
+                phi = b;
+
+                double cosa = cos(a), sina = sin(a);
+                double cosb = cos(b), sinb = sin(b);
+                double cosA = cos(A), sinA = sin(A);
+                double cosB = cos(B), sinB = sin(B);
+
+                double circlex = R + r * cosa;
+                double circley = r * sina;
+
+                x = circlex * (cosB * cosb + sinA * sinB * sinb) - circley * cosA * sinB;
+                y = circlex * (sinB * cosb - sinA * cosB * sinb) + circley * cosA * cosB;
+                z = cosA * circlex * sinb + circley * sinA;
+
+                double by = y / 1.5;
+
+                // position du donut
+                double k = 7;
+                double ooz = 1 / (z + k);
+                xp = (int)(colonne_centre + facteur_zoom * ooz * x);
+                yp = (int)(ligne_centre + facteur_zoom * ooz * by);
+
+                // Calcul de la normale pour la lumière
+                double nx = cosb * cosa;
+                double ny = sinb * cosa;
+                double nz = sina;
+
+                // Direction de la lumière (fixe)
+                double lx = 0;
+                double ly = 1;
+                double lz = -1;
+
+                // Produit scalaire normalisé
+                double norm = sqrt(nx*nx + ny*ny + nz*nz);
+                nx /= norm;
+                ny /= norm;
+                nz /= norm;
+
+                double light_norm = sqrt(lx*lx + ly*ly + lz*lz);
+                lx /= light_norm;
+                ly /= light_norm;
+                lz /= light_norm;
+
+                double L = nx*lx + ny*ly + nz*lz;
+                if (L < 0) L = 0;
+                const char luminance_chars[] = ".,-~:;=!*#$@";
+                int luminance_index = (int)(L * 11);
+                if (luminance_index > 11) luminance_index = 11;
+
+
+                // condition pour que le donut reste dans le tableau screen et ajout d'un caractère
+                if (xp >= 0 && xp < colonne && yp >= 0 && yp < ligne){
+                        if (ooz > zbuffer[yp][xp]) {
+                            zbuffer[yp][xp] = ooz;
+                            screen[yp][xp] = luminance_chars[luminance_index];
+                    }
+                }
+            }
         }
-        putchar('\n');
+
+        // affichage du tableau
+        for (int i = 0; i < ligne; i++) {
+            for (int j = 0; j < colonne; j++) {
+                putchar(screen[i][j]);
+            }
+            putchar('\n');
+        }
+
+        A += 0.04; 
+        B += 0.02;
+
+        usleep(30000);
     }
 }
